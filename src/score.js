dca52031942a823f73d4dae5ac4535a4f31d1530
Staś Małolepszy <stas@duzodobrze.pl>
2017-09-10 03:37:08 +0200

    Fix negative scores

--- src/score.js
+++ src/score.js
@@ -3,7 +3,7 @@ import { distance, dot } from "./math";
 function position_score(target, current, world_size) {
   const dist = distance(target, current);
   const max_distance = Math.sqrt(world_size * world_size) / 2;
-  return 1 - dist / max_distance;
+  return Math.max(0, 1 - dist / max_distance);
 }
 
 function rotation_score(target, current) {
