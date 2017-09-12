b794ccb45aecec0bd8c8d83c69616e64cd55f53d
Staś Małolepszy <stas@duzodobrze.pl>
2017-09-13 00:18:23 +0200

    Add Score taunts

--- src/ScoreScreen.js
+++ src/ScoreScreen.js
@@ -3,6 +3,21 @@ import Scene from "./Scene";
 import { connect } from "./store";
 
 function ScoreScreen({results, index, target}) {
+  const score = results[index];
+  const message = score < 15
+    ? "Doesn't look like it."
+    : score < 35
+    ? "It was something else."
+    : score < 50
+    ? "It's not quite the same."
+    : score < 75
+    ? "Could it be?"
+    : score < 85
+    ? "It reminds you of something."
+    : score < 95
+    ? "That's it, almost there."
+    : "Wonderful. You've found it.";
+
   return Scene(
     {name: "SCENE_SCORE", from: "white", to: "black"},
     html`
@@ -11,7 +26,8 @@ function ScoreScreen({results, index, target}) {
         src="${target.snapshot}">
       <div class="ui action"
         onclick="goto('SCENE_LEVELS')">
-        <div class="pad">${results[index]}/100</div>
+        <div class="pad">${message}</div>
+        <div class="pad">${results[index]}</div>
       </div>`
   );
 }
